d96938d318d3a5da94b3269594b181a57acaad33
pzunigal <132427622+pzunigal@users.noreply.github.com>
2025-02-27 19:33:40 -0300

    Creacion proyecto

---
+++ src/main/java/com/pablo/PeguitasApplication.java
@@ -0,0 +1,13 @@
+package com.pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeguitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeguitasApplication.class, args);
+	}
+
+}
