24fbcfd362108b8d2c82d12cc9679322620eb37a
joaonunez <joaovaldiglesias@gmail.com>
2025-03-07 16:07:27 -0300

    avance en mi servicio de servicios

--- src/main/java/com/servicios/ServicioServicios.java
+++ src/main/java/com/servicios/ServicioServicios.java
@@ -1,5 +1,25 @@
 package com.servicios;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.modelos.Servicio;
+import com.repositorios.RepositorioServicios;
+
+@Service
 public class ServicioServicios {
     
+    //llamamos al repositorio
+    @Autowired
+    private RepositorioServicios repositorioServicios;
+
+    //para obtener todos los servicios de manera general:
+    public List<Servicio> obtenerTodos(){
+        return (List<Servicio>) repositorioServicios.findAll();
+    }
+
+
+
 }
