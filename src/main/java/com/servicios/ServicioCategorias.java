8fafa4a9227d918e1901c15bc4ae5eb43f8737f5
joaonunez <joaovaldiglesias@gmail.com>
2025-03-10 11:01:44 -0300

    servicio categorias avance

--- src/main/java/com/servicios/ServicioCategorias.java
+++ src/main/java/com/servicios/ServicioCategorias.java
@@ -1,5 +1,18 @@
 package com.servicios;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.modelos.Categoria;
+import com.repositorios.RepositorioCategorias;
+
 public class ServicioCategorias {
-    
+    @Autowired
+    private RepositorioCategorias repositorioCategorias;
+
+    //metodo para obtener todas las categorias: (se planea para mostrar todas las cateogiras en el formulario para que asi el usuario pueda seleccionar una opcion)
+    public List<Categoria> obtenerTodas(){
+        return (List<Categoria>) repositorioCategorias.findAll();
+    }
 }
