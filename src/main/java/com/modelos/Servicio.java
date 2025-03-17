0f00033e921f91197a958353703e5ae916c7345c
joaonunez <joaovaldiglesias@gmail.com>
2025-03-17 07:13:53 -0300

    eliminado dependencia innecesaria

--- src/main/java/com/modelos/Servicio.java
+++ src/main/java/com/modelos/Servicio.java
@@ -14,7 +14,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
