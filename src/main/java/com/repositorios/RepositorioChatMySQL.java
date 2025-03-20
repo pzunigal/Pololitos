21e1bc1ec49376f5fcf3d21e86022356f009ba27
joaonunez <joaovaldiglesias@gmail.com>
2025-03-20 20:59:02 -0300

    solucion error critico

--- src/main/java/com/repositorios/RepositorioChatMySQL.java
+++ src/main/java/com/repositorios/RepositorioChatMySQL.java
@@ -2,7 +2,7 @@ package com.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.modelos.Chat;
 
-public interface RepositorioChatMySQL extends JpaRepository<Chat, Long> {
-    // Aquí puedes agregar un método que te permita obtener el chat por solicitudId
+public interface RepositorioChatMySQL extends JpaRepository<Chat, String> {
     Chat findBySolicitudId(Long solicitudId);
 }
+
